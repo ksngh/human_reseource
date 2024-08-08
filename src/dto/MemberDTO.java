25281013f12c97c01dd652ed87e4b4bb97b56f62
ksngh <chzhqk98@naver.com>
2024-08-08 16:14:30 +0900

    Chore : entity,dto 추가

--- src/dto/MemberDTO.java
+++ src/dto/MemberDTO.java
@@ -1,4 +1,46 @@
 package dto;
 
 public class MemberDTO {
+	String userName;
+	String deptName;
+	String date;
+	String status;
+
+	public MemberDTO(String userName, String date, String status) {
+		this.userName = userName;
+		this.date = date;
+		this.status = status;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
--- src/entity/Member.java
+++ src/entity/Member.java
@@ -1,4 +1,20 @@
 package entity;
 
 public class Member {
+
+	public Member() {
+
+	}
+
+	public Member(String userName, String deptName, String date, String status) {
+		this.userName = userName;
+		this.deptName = deptName;
+		this.date = date;
+		this.status = status;
+	}
+
+	String userName;
+	String deptName;
+	String date;
+	String status;
 }
