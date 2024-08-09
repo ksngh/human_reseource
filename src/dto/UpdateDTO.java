5d5aa2490d37c1d78cbb626328ffba6e6436dfa3
ksngh <chzhqk98@naver.com>
2024-08-09 10:58:20 +0900

    Feat : 근태 수정 I/O 추가 및 불필요한 메서드 주석처리

---
+++ src/dto/UpdateDTO.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class UpdateDTO {
+	String id;
+	String date;
+	String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
